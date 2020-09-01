7a9bc129d004c74eee017e9b2f24217d7717e4a8
Slipeer13 <68935675+Slipeer13@users.noreply.github.com>
2020-09-01 10:53:58 +0500

    Create Solution.java

---
+++ JavaSyntax/task/task07/task0703/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task07.task0703;                                                                                                    
+import java.io.BufferedReader;                                                  
+import java.io.InputStreamReader;                                                                                                   
+/*                                                                                                     
+Общение одиноких массивов                                                                                                    
+*/                                                                                                    
+                                                                                                    
+public class Solution {                                                                                                    
+    public static void main(String[] args) throws Exception {                                                                                                    
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));                                                  
+        String[] list = new String[10];                                                  
+        int[] array = new int[10];                                                  
+        for (int i = 0; i < 10; i++) {                                                  
+        list[i] = buff.readLine();                                                  
+        array[i] = list[i].length();                                                  
+        System.out.println(array[i]);                                                  
+        }//напишите тут ваш код                                                    
+                                                                                                          
+    }                                                                                                    
+}                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  

0053f27d864fbd19a1ba154d6877acb3e317a0d8
Slipeer13 <68935675+Slipeer13@users.noreply.github.com>
2020-09-03 16:13:10 +0500

    Create Solution.java

---
+++ JavaSyntax/task/task07/task0710/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task07.task0710;                                                                                                                                                                                                                                                          
+import java.io.BufferedReader;                                                                                                                                                      
+import java.io.InputStreamReader;                                                   
+import java.util.ArrayList;                                                                                                                                                                                                                                                        
+/*                                                                                                                                                                                                                                                           
+В начало списка                                                                                                                                                                                                                                                          
+*/                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                          
+public class Solution {                                                                                                                                                                                                                                                          
+    public static void main(String[] args) throws Exception {                                                                                                                                                       
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));                                                                                                                                                                                                                                                         
+        ArrayList<String> string = new ArrayList<String>();                                                                                                                                                       
+        for (int i = 0; i < 10; i++) {                                                                                                                                                      
+           string.add(0 , buff.readLine());                                                                                                      
+                                                                                                                                                             
+        }                                                     
+        for (int i = 0; i < 10; i++) {                                                                                                                                                      
+           System.out.println(string.get(i));                                                                                                      
+                                                                                                                                                             
+        }                                                                                                                                                                                  
+//напишите тут ваш код                                                                                                                                                                                                                                                          
+    }                                                                                                                                                                                                                                                          
+}                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
